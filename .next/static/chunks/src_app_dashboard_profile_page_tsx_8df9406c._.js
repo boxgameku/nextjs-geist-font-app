6c65ad41ac9af7a609826d6e25982bbb88f2ad27
boxgameku <boxgameku@gmail.com>
2025-06-15 21:24:30 +0700

    Add .next/static/chunks/src_app_dashboard_profile_page_tsx_8df9406c._.js 1749997469084

---
+++ .next/static/chunks/src_app_dashboard_profile_page_tsx_8df9406c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_1a3d6396._.js"
+],
+    source: "dynamic"
+});
